813e42217fe36d4a7a795cb6f5896f0f6b6a91aa
yusraimran2 <yusraimran12@hotmail.com>
2024-11-12 20:49:16 +0500

    Add files via upload

---
+++ Q7.js
@@ -0,0 +1,18 @@
+"use strict";
+//7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+add(5, 3);
+function sub(num1, num2) {
+    console.log(num1 - num2);
+}
+sub(10, 2);
+function multi(num1, num2) {
+    console.log(num1 * num2);
+}
+multi(4, 2);
+function divi(num1, num2) {
+    console.log(num1 / num2);
+}
+divi(64, 8);
